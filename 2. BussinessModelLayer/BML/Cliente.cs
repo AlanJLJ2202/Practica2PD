c15590fc04d5b387b87abf1a8d76e9bfbb3c08d8
AlanJLJ2202 <lrs19110248@purisima.tecnm.mx>
2022-04-26 21:11:21 -0500

    Se agregaron clases (Clientes, Ventas) en BML

---
+++ 2. BussinessModelLayer/BML/Cliente.cs
@@ -0,0 +1,84 @@
+﻿using DAL;
+using Dapper;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BML
+{
+    public class Cliente
+    {
+        public DataAccess dataAccess = DataAccess.Instance();
+
+        public int idCliente { get; set; }
+
+        public string nombre { get; set; }
+
+        public string apMaterno { get; set; }
+
+        public string apPaterno { get; set; }
+
+        public string direccion { get; set; }
+
+        public string telefono { get; set; }
+
+        public string cp { get; set; }
+
+        public bool activo { get; set; }
+
+       
+        public Cliente() { }
+
+
+        public int Add()
+        {
+            var parameters = new DynamicParameters();
+            parameters.Add("@nombre", nombre);
+            parameters.Add("@apMaterno", apMaterno);
+            parameters.Add("@apPaterno", apPaterno);
+            parameters.Add("@direccion", direccion);
+            parameters.Add("@telefono", telefono);
+            parameters.Add("@cp", cp);
+            return dataAccess.Execute("stp_clientes_add", parameters);
+        }
+
+
+
+        public int Delete()
+        {
+            var parameters = new DynamicParameters();
+            parameters.Add("@idCliente", idCliente);
+            return dataAccess.Execute("stp_clientes_delete", parameters);
+        }
+
+
+        public int Update()
+        {
+            var parameters = new DynamicParameters();
+            parameters.Add("@idCliente", idCliente);
+            parameters.Add("@nombre", nombre);
+            parameters.Add("@apMaterno", apMaterno);
+            parameters.Add("@apPaterno", apPaterno);
+            parameters.Add("@direccion", direccion);
+            parameters.Add("@telefono", telefono);
+            parameters.Add("@cp", cp);
+            return dataAccess.Execute("stp_clientes_update", parameters);
+        }
+
+
+        public IEnumerable<Cliente> GetAll()
+        {
+            return dataAccess.Query<Cliente>("stp_clientes_getall");
+        }
+
+
+        public Cliente GetById()
+        {
+            var parameters = new DynamicParameters();
+            parameters.Add("@idCliente", idCliente);
+            return dataAccess.QuerySingle<Cliente>("stp_clientes_getbyid", parameters);
+        }
+    }
+}
---
+++ 2. BussinessModelLayer/BML/Ventas.cs
@@ -0,0 +1,98 @@
+﻿using DAL;
+using Dapper;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BML
+{
+    public class Ventas
+    {
+        public DataAccess dataAccess = DataAccess.Instance();
+
+
+        public int idVenta { get; set; }
+
+        public int folio { get; set; }
+
+        public DateTime fecha { get; set; }
+
+        public int cantidad { get; set; }
+
+        public decimal importe { get; set; }
+
+        public int descuento { get; set; }
+
+        public decimal total { get; set; }
+
+        public string estatus { get; set; }
+
+        public int idCliente { get; set; }
+
+        public int idUsuario { get; set; }
+
+        public bool activo { get; set; }
+
+
+        public Ventas() { }
+
+
+        public int Add()
+        {
+            var parameters = new DynamicParameters();
+            parameters.Add("@folio", folio);
+            parameters.Add("@fecha", fecha);
+            parameters.Add("@cantidad", cantidad);
+            parameters.Add("@importe", importe);
+            parameters.Add("@descuento", descuento);
+            parameters.Add("@total", total);
+            parameters.Add("@estatus", estatus);
+            parameters.Add("@idCliente", idCliente);
+            parameters.Add("@idUsuario", idUsuario);
+            return dataAccess.Execute("stp_ventas_add", parameters);
+        }
+
+
+
+        public int Delete()
+        {
+            var parameters = new DynamicParameters();
+            parameters.Add("@idVenta", idVenta);
+            return dataAccess.Execute("stp_ventas_delete", parameters);
+        }
+
+
+        public int Update()
+        {
+            var parameters = new DynamicParameters();
+            parameters.Add("@idVenta", idVenta);
+            parameters.Add("@folio", folio);
+            parameters.Add("@fecha", fecha);
+            parameters.Add("@cantidad", cantidad);
+            parameters.Add("@importe", importe);
+            parameters.Add("@descuento", descuento);
+            parameters.Add("@total", total);
+            parameters.Add("@estatus", estatus);
+            parameters.Add("@idCliente", idCliente);
+            parameters.Add("@idUsuario", idUsuario);
+            return dataAccess.Execute("stp_ventas_update", parameters);
+        }
+
+
+        public IEnumerable<Ventas> GetAll()
+        {
+            return dataAccess.Query<Ventas>("stp_ventas_getall");
+        }
+
+
+        public Ventas GetById()
+        {
+            var parameters = new DynamicParameters();
+            parameters.Add("@idVenta", idVenta);
+            return dataAccess.QuerySingle<Ventas>("stp_ventas_getbyid", parameters);
+        }
+
+    }
+}
